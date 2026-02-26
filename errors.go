c92267682b511aa4a79081c7a588eed1f89e97c0
sekthor <me@sekthor.dev>
2026-02-26 22:50:58 +0100

    feat: add Marshal/Unmarshal, remove MatchInto

--- errors.go
+++ errors.go
@@ -6,4 +6,5 @@ var (
 	ErrSegmentLengthMismatch   = errors.New("resource name segment count does not match that of the pattern")
 	ErrSegmentConstantMismatch = errors.New("a non-variable segment did not match constant pattern segment")
 	ErrNotAStruct              = errors.New("resource must be a pointer to a struct")
+	ErrMissingSegment          = errors.New("resource is missing variable segment")
 )
--- tags.go
+++ tags.go
@@ -1,13 +1,16 @@
 package resourcenames
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 // MatchInto parses a given resource name with the name pattern.
 // All discovered variable segment values will be set on the corresponding
 // resource field.
 // It chooses the field to set the value to by comparing the segment name
-// with the matching value in the `rns:"<segment_name>§"` tag.
-func (p NamePattern) MatchInto(resourceName string, resource any) error {
+// with the matching value in the `rns:"<segment_name>"` tag.
+func (p NamePattern) Unmarshal(resourceName string, resource any) error {
 	params, err := p.Parse(resourceName)
 	if err != nil {
 		return err
@@ -47,4 +50,65 @@ func (p NamePattern) MatchInto(resourceName string, resource any) error {
 	}
 
 	return nil
+}
+
+func (p NamePattern) Marshal(resource any) (string, error) {
+	resourceName := ""
+	val := reflect.ValueOf(resource)
+
+	if val.Kind() == reflect.Pointer {
+		if val.IsNil() {
+			return resourceName, ErrNotAStruct
+		}
+		val = val.Elem()
+	}
+
+	if val.Kind() != reflect.Struct {
+		return resourceName, ErrNotAStruct
+	}
+
+	typ := val.Type()
+
+	for _, segment := range p.segments {
+		if !segment.isParam {
+			resourceName += "/" + segment.value
+			continue
+		}
+
+		found := false
+
+		for i := 0; i < typ.NumField(); i++ {
+			field := typ.Field(i)
+			fieldValue := val.Field(i)
+
+			if field.Tag.Get("rns") != segment.value {
+				continue
+			}
+
+			// Convert field value to string
+			var str string
+
+			switch fieldValue.Kind() {
+			case reflect.String:
+				str = fieldValue.String()
+			default:
+				str = fmt.Sprint(fieldValue.Interface())
+			}
+
+			resourceName += "/" + str
+			found = true
+			break
+		}
+
+		if !found {
+			return "", fmt.Errorf("%w: %q", ErrMissingSegment, segment.value)
+		}
+	}
+
+	// Optional: strip leading slash if your format requires
+	if resourceName == "" {
+		return "", nil
+	}
+
+	return resourceName, nil
 }
